57ac7438a8cea00ed4a42b460d065fc159f02aa1
Pugsly99 <rylandadams@yahoo.com>
2020-11-06 11:06:47 -0800

    html for taking in inputs

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,12 @@ $(document).ready(function() {
     event.preventDefault();
 
     let size = parseInt($("select#size").val())
-    let toppings = parseInt($("form#toppings").val())
+    let sauce = parseInt($("select#sauce").val())
+    let cheese = parseInt($("select#cheese").val())
+    let extra1 = parseInt($("select#extra1").val())
+    let extra2 = parseInt($("select#extra2").val())
+    let extra3 = parseInt($("select#extra3").val())
+    
 
     console.log(size);
     console.log(toppings);
