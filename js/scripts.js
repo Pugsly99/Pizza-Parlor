ca1f1616445cf472af93f3ead12c3486d09219ea
Pugsly99 <rylandadams@yahoo.com>
2020-11-11 14:05:55 -0800

    fixing up my javascript to match my new html

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,57 @@
 //Business Logic
-function Pizza() {
+function Pizza(size, sauce, cheese, toppings) {
+  this.size = size;
+  this.sauce = sauce;
+  this.cheese = cheese;
+  this.toppings = toppings;
   this.price = 0;
-  this.size;
-  this.toppings;
 }
 
-Pizza.prototype.addValue = function (size, sauce, cheese, extra1, extra2, extra3) {
-  let price = size + sauce + cheese + extra1 + extra2 + extra3
-  this.price = price;
-}
+// Pizza.prototype.addValue = function (size, sauce, cheese, extra1, extra2, extra3) {
+//   let price = size + sauce + cheese + extra1 + extra2 + extra3
+//   this.price = price;
+// }
 
-Pizza.prototype.size = function (size) {
-  if(size === 10){
-    this.size = "Small";
-  } else if(size === 15){
-    this.size = "Medium";
-  } else if(size === 20){
-    this.size = "Large"; 
+Pizza.prototype.price = function () {
+  if(this.size === "Small"){
+    this.price = 10;
+  } else if(this.size === "Medium"){
+    this.price = 15;
+  } else if(this.size === "Large"){
+    this.price = 20; 
   } else {
     window.alert("yoooo pick a size, you cant have a pizza without the crust");
     return false;
   }   
+  if (this.sauce === "Marinara") {
+    this.price += 1;
+  } else if (this.sauce === "Pesto")  {
+    this.price += 1;
+  } else if (this.sauce === "White Garlic") {
+    this.price += 1;
+  }
+  this.price += this.toppings.length;
+  console.log(this.toppings.length);
+  return this.price;
 }
 
-Pizza.prototype.showPizza = function(cheesename, saucename, extra1name, extra2name, extra3name) {
-  $("#myPizza").append("Your " + this.size + " " + cheesename + " with " + saucename + " sauce and extras that are " + extra1name + " " + extra2name + " " + extra3name + " will be ready soon! Please send " + this.price + "$ to Rylandadams@yahoo.com");
+// Pizza.prototype.showPizza = function(cheesename, saucename, extra1name, extra2name, extra3name) {
+//   $("#myPizza").append("Your " + this.size + " " + cheesename + " with " + saucename + " sauce and extras that are " + extra1name + " " + extra2name + " " + extra3name + " will be ready soon! Please send " + this.price + "$ to Rylandadams@yahoo.com");
+// }
+
+function OrderMaker() {
+  let size = $("option[name=size]:selected").val();
+  let sauce = $("option[name=sauce]:selected").val();
+  let cheese = $("option[name=cheese]:selected").val();
+  let toppings = []
+  $("option[name=topping]:selected").each(function() {
+    toppings.push($(this).val());
+  });
+  let thisPizza = new Pizza(size, sauce, cheese, toppings);
+  let price = thisPizza.price();
+  //thisPizza.getPrice();
+  //let price = thisPizza.getPrice();
+  return thisPizza;
 }
 
 //UI logic
@@ -34,7 +61,8 @@ $(document).ready(function() {
 
     $( "h2").empty();
 
-    let pizza = new Pizza();
+    let thisPizza = new orderMaker();
+    console.log(thisPizza)
     const size = parseInt($("select#size").val());
 
     const sauce = parseInt($("select#sauce").val());
